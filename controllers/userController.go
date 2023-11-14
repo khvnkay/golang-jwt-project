c894734730d111e5c72d5f24993743e0f18093c5
krongkan <krongkan.madjaroensuk@ibm.com>
2023-11-14 16:22:00 +0700

    continue

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,9 +1,17 @@
 package controllers
 
 import (
+	"context"
 	"khvnkay/golang-jwt-project/database"
+	helper "khvnkay/golang-jwt-project/helpers"
+	"khvnkay/golang-jwt-project/models"
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -18,7 +26,29 @@ func veriryPassword() {
 
 }
 
-func signUp() {
+func signUp() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validateError := validate.Struct(user)
+		if validateError != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validateError.Error()})
+			return
+		}
+		count, err := userCollaction.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+
+	}
 
 }
 func Login() {
@@ -28,6 +58,26 @@ func Login() {
 func GetUsers() {
 
 }
-func GetUser() {
+func GetUser() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
+
+		if err := helper.MatchUserYpeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		err := userCollaction.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
+
+	}
 
 }
